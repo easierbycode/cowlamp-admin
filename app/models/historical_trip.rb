11414fd9188583ddc2ca29a48e5ba35001f9c9cb
Daniel Johnson <navseeker7@gmail.com>
2013-05-12 18:21:24 -0700

    initial commit

---
+++ app/models/historical_trip.rb
@@ -0,0 +1,3 @@
+class HistoricalTrip < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GatewayAa::Application.config.secret_token = '717e2d824da9b44e7c6672e94f8e19b360d6e82d66b5eca76d513fd73e6ff5464a47f05d48d3bf0e07db45eda250910513bb73864721aa41571b1504d33500a6'
