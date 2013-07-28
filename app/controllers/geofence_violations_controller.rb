7ebafec1d4409b8462a5eea4e1551dcff6204508
Daniel Johnson <navseeker7@gmail.com>
2013-07-27 19:56:49 -0700

    fix geofence_violation

--- app/controllers/geofence_violations_controller.rb
+++ app/controllers/geofence_violations_controller.rb
@@ -1,5 +1,8 @@
 class GeofenceViolationsController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+  respond_to :json
+
   def create
     respond_with GeofenceViolation.create(params[:geofence_violation])
   end
