8311609bbd22605164637648f734c4810da2cf5f
Daniel Johnson <navseeker7@gmail.com>
2013-08-01 14:29:14 -0700

    fix geofence_boundary check logic

--- app/models/historical_trip.rb
+++ app/models/historical_trip.rb
@@ -3,7 +3,7 @@ class HistoricalTrip < ActiveRecord::Base
   attr_protected :created_at
 
   before_save :add_trip_number
-  #before_save :check_geofence_violations
+  before_save :check_geofence_violations
 
   def check_geofence_violations
     self.has_geofence_violations = boundary_violations?
@@ -16,17 +16,18 @@ class HistoricalTrip < ActiveRecord::Base
   # check to see if
   # we exited any boundaries
   def boundary_violations?
-    geofence_violations = GeofenceViolation.where({ trip_start_at:start_at })
-    geofence_ids = geofence_violations.pluck('geofence_id')
-    violated_a_boundary = false
+    if geofence_violations = GeofenceViolation.where({ trip_start_at:start_at })
+      geofence_ids = geofence_violations.pluck('geofence_id')
+      violated_a_boundary = false
 
-    for geofence_id in geofences_ids
-      if Geofence.is_boundary(geofence_id)
-        violated_a_boundary = true
+      for geofence_id in geofences_ids
+        if Geofence.is_boundary(geofence_id)
+          violated_a_boundary = true
+        end
       end
-    end
 
-    return violated_a_boundary
+      return violated_a_boundary
+    end
   end
 
 end
